e7b6abad0742f8395d243c8d45a785a703038c2b
Hanna Yakush <hanna.yakush@jetbrains.com>
2024-11-07 11:50:06 +0100

    Add imports

--- src/PrestaShopBundle/Entity/Attribute.php
+++ src/PrestaShopBundle/Entity/Attribute.php
@@ -5,6 +5,8 @@ namespace PrestaShopBundle\Entity;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Doctrine\ORM\Mapping\JoinColumn;
+use Symfony\Component\HttpFoundation\Request;
 
 /**
  * Attribute.
